e087328a9a5ce63ba21192b235421255003098b4
hypnodroid <iam@aaronherres.com>
2024-10-08 03:11:47 -0700

    widget

--- image_selector_node.py
+++ image_selector_node.py
@@ -2,11 +2,12 @@ import torch
 
 class ImageSelector:
     CATEGORY = "example"
-    @classmethod
-    def INPUT_TYPES(s):
-        return { "required":  { "images": ("IMAGE",), } }
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "choose_image"
+    @classmethod
+    def INPUT_TYPES(s):
+        return { "required":  { "images": ("IMAGE",),
+                                "mode": (["brightest", "reddest", "greenest", "bluest"],)} }
 
     def choose_image(self, images):
         brightness = list(torch.mean(image.flatten()).item() for image in images)
